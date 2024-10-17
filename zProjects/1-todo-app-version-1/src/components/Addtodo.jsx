45324beae8ec8e579c912b234e0c5acb9b47a994
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-10-17 21:42:19 +0530

    Commit

---
+++ zProjects/1-todo-app-version-1/src/components/Addtodo.jsx
@@ -0,0 +1,21 @@
+function Addtodo() {
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-6">
+          <input type="text" name="" Placeholder="Enter todo Here" />
+        </div>
+        <div class="col-4">
+          <input type="date" />{" "}
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-success">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Addtodo;
