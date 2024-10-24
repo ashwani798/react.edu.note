b184d8465bcc4de884f023207972bd494af54be0
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-10-24 19:11:25 +0530

    Commit

--- zProjects/7-social-media/src/App.jsx
+++ zProjects/7-social-media/src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Placeholder</div>
+      <header class="p-3 text-bg-dark">
+        <div class="container">
+          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a
+              href="/"
+              class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+            >
+              <svg
+                class="bi me-2"
+                width="40"
+                height="32"
+                role="img"
+                aria-label="Bootstrap"
+              >
+                <use xlink:href="#bootstrap"></use>
+              </svg>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li>
+                <a href="#" class="nav-link px-2 text-secondary">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link px-2 text-white">
+                  Features
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link px-2 text-white">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link px-2 text-white">
+                  FAQs
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link px-2 text-white">
+                  About
+                </a>
+              </li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+              <input
+                type="search"
+                class="form-control form-control-dark text-bg-dark"
+                placeholder="Search..."
+                aria-label="Search"
+              />
+            </form>
+
+            <div class="text-end">
+              <button type="button" class="btn btn-outline-light me-2">
+                Login
+              </button>
+              <button type="button" class="btn btn-warning">
+                Sign-up
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
